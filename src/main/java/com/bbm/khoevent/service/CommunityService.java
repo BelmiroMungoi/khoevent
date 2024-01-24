1150695d48ab4cc4c3d5dedbd7a7814806384e85
Belmiro Mungoi <belmiromungoi@gmail.com>
2024-01-24 21:53:13 +0200

    Adding new methods

--- src/main/java/com/bbm/khoevent/service/CommunityService.java
+++ src/main/java/com/bbm/khoevent/service/CommunityService.java
@@ -1,8 +1,16 @@
 package com.bbm.khoevent.service;
 
 import com.bbm.khoevent.dto.request.CommunityRequest;
+import com.bbm.khoevent.dto.response.CommunityResponse;
+import com.bbm.khoevent.model.Community;
+
+import java.util.List;
 
 public interface CommunityService {
 
     String createCommunity(CommunityRequest request);
+    List<CommunityResponse> findAllCommunities();
+    CommunityResponse findById(Long id);
+    Community getCommunityById(Long id);
+    String update(CommunityRequest communityRequest, Long id);
 }
